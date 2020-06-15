482b90b26807dfae90b45f7a2bdcb14090606460
barathviknesh <57832162+barathviknesh@users.noreply.github.com>
2020-06-15 14:16:19 +0530

    Add files via upload

---
+++ miniex3.py
@@ -0,0 +1,13 @@
+#numbers=[5,2,5,2,2]
+#for x in numbers:
+#    print("x"*x)
+
+#secound method
+
+
+numbers=[5,2,5,2,2]
+for x in numbers:
+    output=""
+    for count in range(x):
+        output=output+"x"
+    print(output)
